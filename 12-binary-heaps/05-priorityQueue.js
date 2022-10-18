59628f6d459700e2b02f46dee5333119eef898af
Chenjia Li <cligrenet@gmail.com>
2022-10-18 12:28:31 +0200

    Intro and implement priority queue

---
+++ 12-binary-heaps/05-priorityQueue.js
@@ -0,0 +1,87 @@
+// Priority Queue
+// A data structure where each element has a priority.
+// Element with higher priorities are served before elements with lower priorities.
+
+class Node {
+	constructor(val, priority) {
+		this.val = val;
+		this.priority = priority;
+	}
+}
+
+// kind of MinBinaryHeap
+class PriorityQueue {
+	constructor() {
+		this.values = [];
+	}
+	enqueue(val, priority) {
+		let newNode = new Node(val, priority);
+		this.values.push(newNode);
+		this.bubbleUp();
+	}
+	bubbleUp() {
+		let idx = this.values.length - 1;
+		const element = this.values[idx];
+		while (idx > 0) {
+			let parentIdx = Math.floor((idx - 1) / 2);
+			let parent = this.values[parentIdx];
+			if (element.priority >= parent.priority) break;
+			this.values[parentIdx] = element;
+			this.values[idx] = parent;
+			idx = parentIdx;
+		}
+	}
+
+	dequeue() {
+		const min = this.values[0];
+		const end = this.values.pop();
+		if (this.values.length > 0) {
+			this.values[0] = end;
+			this.sinkDown();
+		}
+		return min;
+	}
+	sinkDown() {
+		let idx = 0;
+		const length = this.values.length;
+		const element = this.values[0];
+
+		while (true) {
+			let leftChildIdx = 2 * idx + 1;
+			let rightChildIdx = 2 * idx + 2;
+			let leftChild, rightChild;
+			let swap = null; // either the left child index or the right child index
+
+			if (leftChildIdx < length) {
+				leftChild = this.values[leftChildIdx];
+				if (leftChild.priority < element.priority) {
+					swap = leftChildIdx;
+				}
+			}
+			if (rightChildIdx < length) {
+				rightChild = this.values[rightChild];
+				if (
+					(swap === null && rightChild.priority < element.priority) ||
+					(swap !== null && rightChild.priority < leftChild.priority)
+				) {
+					swap = rightChildIdx;
+				}
+			}
+			if (swap === null) break;
+
+			this.values[idx] = this.values[swap];
+			this.values[swap] = element;
+			idx = swap;
+		}
+	}
+}
+
+let ER = new PriorityQueue();
+ER.enqueue('Common Cold', 5);
+ER.enqueue('Gunshot wound', 1);
+ER.enqueue('High fever', 4);
+// console.log(ER);
+console.log(ER.dequeue()); // 'Gunshot wound'
+console.log(ER.dequeue()); // 'High fever'
+console.log(ER.dequeue()); // 'Common Cold'
+console.log(ER.dequeue());
