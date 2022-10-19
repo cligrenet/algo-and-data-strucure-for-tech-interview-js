c5dbeb0326b07f6dd8188222afd2b39299aef76e
Chenjia Li <cligrenet@gmail.com>
2022-10-19 09:48:06 +0200

    Intro to hash table

---
+++ 13-hash-tables/00-intro.js
@@ -0,0 +1,15 @@
+// Hash Tables
+// - Store key-value pairs
+// - The keys are not ordered
+// - Fast for finding values, adding new values, removing values
+
+// Python - dictionary // JS - object / map // Java, Go , Scala - map // Ruby - hash
+
+// Collision in a hash table
+
+// Solutions:
+// - Seperate chaining
+// - Linear probing
+
+// Complexity
+// constant time
