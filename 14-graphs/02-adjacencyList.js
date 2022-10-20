3473eb351babcb9be8515ad1581733e6a5db8488
Chenjia Li <cligrenet@gmail.com>
2022-10-20 10:16:37 +0200

    Intro to adjacency list

---
+++ 14-graphs/02-adjacencyList.js
@@ -0,0 +1,46 @@
+/*
+Adjacency List:
+- 🤟 Can take up less space (in sparse graphs)
+- 🤟 Faster to iterate over all edges
+- Can be slower to loopup specific edge
+
+Complexity:
+  V = # of vertices
+  E = # of edges
+Add vertex       O(1)  
+Add edge         O(1)             
+Remove vertex    O(V+E)            
+Remove edge      O(E)              
+Query            O(V+E)            
+Storage          O(V+E)            
+
+                0 ---- 1
+               /         \ 
+              5           2
+               \         /
+                 4 ---- 3 
+
+    [
+ 0      [1,5],
+ 1      [0,2],
+ 2      [1,3],
+ 3      [2,4],
+ 4      [3,5],
+ 5      [4,0]
+    ]
+
+                A ---- B
+               /         \ 
+              F           c
+               \         /
+                 E ---- D  
+
+    {
+        A: ["B", "F"],
+        B: ["A", "C"],
+        C: ["B", "D"],
+        D: ["C", "E"],
+        E: ["D", "F"],
+        F: ["E", "A"]
+    }             
+*/
