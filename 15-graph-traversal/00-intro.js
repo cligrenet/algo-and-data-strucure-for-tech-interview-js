57e57269e41983f81b012323fada63b7ffe91827
Chenjia Li <cligrenet@gmail.com>
2022-10-21 10:15:34 +0200

    Intro to graph traversal

---
+++ 15-graph-traversal/00-intro.js
@@ -0,0 +1,15 @@
+// Graph Traversal
+// Visiting, updating, checking each vertex in a graph
+
+// Use cases:
+// - Peer to peer networking
+// - Web crawlers
+// - Finding "closest" matches/ recommendations
+// - Shortest path problems (GPS navigation, solving mazes, AI-shortest path to win the game...)
+
+// 1- Depth First Graph Traversal (DFS) - recursive and iterative
+//// Move away from the root
+//// Tree: Deepen the traversal before widen it (visit children before siblings)
+//// Graph: Follow a branch down
+
+// 2- Breadth First Graph Traversal (BFS)
