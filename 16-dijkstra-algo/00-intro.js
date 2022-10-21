0a216179fa1c63de4f3dbe0793a6a4cea446626b
Chenjia Li <cligrenet@gmail.com>
2022-10-21 10:42:19 +0200

    Intro to Dijkstra's algo

---
+++ 16-dijkstra-algo/00-intro.js
@@ -0,0 +1,9 @@
+// Dijkstra's Algo (the shortest path)
+// Weighted graph + binary heap priority queue
+
+// Use cases
+// - GPS, finding fastest route
+// - Network routing, finds open shortest path for data
+// - Biology, used to model the spread of viruses among humans
+// - Airline tickets, finding cheapest route to your destination
+// ...
