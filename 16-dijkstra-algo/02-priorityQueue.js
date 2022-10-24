7a10c84cb5e77dedc7af72a5a4d3d5106425247f
Chenjia Li <cligrenet@gmail.com>
2022-10-24 09:46:57 +0200

    Write naive version of priority queue

---
+++ 16-dijkstra-algo/02-priorityQueue.js
@@ -0,0 +1,34 @@
+// V1 naive version
+class PriorityQueue {
+	constructor() {
+		this.values = [];
+	}
+	enqueue(val, priority) {
+		this.values.push({ val, priority });
+		this.sort();
+	}
+	dequeue() {
+		return this.values.shift();
+	}
+	sort() {
+		this.values.sort((a, b) => a.priority - b.priority);
+	}
+}
+
+// sort => O(N*log(N))
+
+let q = new PriorityQueue();
+q.enqueue('B', 3);
+q.enqueue('C', 5);
+q.enqueue('D', 2);
+q.enqueue('Q', 20);
+console.log(q.values);
+/* 
+[
+  { val: 'D', priority: 2 },
+  { val: 'B', priority: 3 },
+  { val: 'C', priority: 5 },
+  { val: 'Q', priority: 20 }
+]
+*/
+console.log(q.dequeue()); //{ val: 'D', priority: 2 }
