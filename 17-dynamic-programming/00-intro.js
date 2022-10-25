032d5e418255094fb61a0f18061e2747a56e778d
Chenjia Li <cligrenet@gmail.com>
2022-10-25 09:40:35 +0200

    Intro to dynamic programming

---
+++ 17-dynamic-programming/00-intro.js
@@ -0,0 +1,18 @@
+// Dynamic Programming basics
+// 1- overlapping subproblems
+// 2- optimal structure
+
+// Overlapping subproblems:
+// - Fibonacci sequence: 1, 1, 2, 3, 5, 8, 13...
+/*
+                fib(5)
+            /           \
+        fib(4)   +     fib(3️⃣) 
+        /   \          /    \
+   fib(3️⃣) + fib(2)   fib(2) + fib(1)
+    /   \
+fib(2) + fib(1)
+ 
+*/
+
+// Optimal structure:
